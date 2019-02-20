c1467ae11e5d270c5f1fb50f798591de99e9d578
vijay durga <vijaydurga3@gmail.com>
2019-02-20 21:45:33 +0530

    factorial

---
+++ factorial_1/factorial_1/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int fact(int);
+int main()
+{
+	int n, fac;
+	cout << "enter number";
+	cin >> n;
+	fac = fact(n);
+	cout << fac;
+	system("pause");
+}
+int fact(int n)
+{
+	if (n == 1)
+		return 1;
+	else
+		return(n*fact(n - 1));
+}
